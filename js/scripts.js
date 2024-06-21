36c25c1fb9c2a64943ade1c0c9129ee1c070a63c
Tawachiko <167883689+Tawachiko@users.noreply.github.com>
2024-06-21 16:28:56 +0900

    変更した

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+
+<!--BEGIN SENTRY-->
+<script type="text/javascript" async src="https://analytics.tiktok.com/i18n/pixel/static/identify ce1d8843.js">
+ </script>
+
+<script type="text/javascript" async src="https://analytics.tiktok.com/i18n/pixel/static/main.MTU0NDc1MDUxMQ.js" data-id="C3SIOCO4C3S2QIOV7NIO">
+ </script>
+
+<script  scr="https://s.yimg.com/wi/ytc.js" async>
+ </script>
+
+<script scr="https://connect.facebook.net/signals/config/704136006388169?v=2.9.158/"
+<script scr="//connect.facebook.net/en US/fbevents.js">
+ </script>
+
+　　<script async scr="//connect.facebook.net/en_US/fbevents.js">
+ </script>
+
+<script type="text/javascript" async src="https://static.ads-twitter.com/uwt.js">
+</script>
+
+<script type="text/javascript" async src="https://www.googletagmanager.com/gtag/destination?id=AW-1041825691&l=datalayer&cx=c">
+</script>
+
+<script type="text/javascript" async src="https://www.googletagmanager.com/gtag/destination?id=AW-1006927621&amp;l=dataLayer&amp;cx=c">
+</script>
+
+<script type="text/javascript" async src="https://bat.bing.com/bat.js">
+</script>
+
+<script type="text/javascript" async src="https://www.googletagmanager.com/gtag/destination?id=DC-4382365&l=dataLayer&cx=c">
+</script>
+
+<script 
+<!--END SENTRY-->
